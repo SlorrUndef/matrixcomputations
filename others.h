8ede3b0d287413d6344b6891183bdd999af4fb59
Mansur <50706718+SlorrUndef@users.noreply.github.com>
2022-05-24 14:59:25 +0300

    Delete others.h

--- others.h
+++
@@ -1,7 +0,0 @@
-void generator(double *a, int n, int k, const std::string &filename);
-
-double residual(double *a, double *x, int n);
-
-double get_full_time();
-
-void outputArray(int n, int m, int l, double *a);
