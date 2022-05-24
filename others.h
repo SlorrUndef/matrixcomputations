17faac9afceef31beab7f2b312d7ae6d3ad8ffa1
Mansur <50706718+SlorrUndef@users.noreply.github.com>
2022-05-24 14:57:43 +0300

    Add files via upload

---
+++ others.h
@@ -0,0 +1,7 @@
+void generator(double *a, int n, int k, const std::string &filename);
+
+double residual(double *a, double *x, int n);
+
+double get_full_time();
+
+void outputArray(int n, int m, int l, double *a);
